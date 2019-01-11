678b31d94a4af88ed2706fa93684f9a45225c38b
DK\AEC <aec@sitecore.net>
2019-01-11 17:25:48 +0200

    Initial commit

---
+++ src/Sitecore.Support.303772/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.303772")]
+[assembly: AssemblyProduct("Sitecore.Support.303772")]
+[assembly: ComVisible(false)]
